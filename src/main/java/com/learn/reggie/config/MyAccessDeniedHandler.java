4407ad7b70005dc34ed8ae44748e405e5a6235c2
login <1254581982@qq.com>
2022-04-26 21:49:48 +0800

    整合SpringSecurity

---
+++ src/main/java/com/learn/reggie/config/MyAccessDeniedHandler.java
@@ -0,0 +1,25 @@
+package com.learn.reggie.config;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+        httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        httpServletResponse.setContentType("application/json;charset=utf-8");
+        PrintWriter writer = httpServletResponse.getWriter();
+        writer.write("{\"status\":\"403\",\"msg\":\"权限不足，无法进入\"}");
+        writer.flush();
+        writer.close();
+    }
+}
---
+++ src/main/java/com/learn/reggie/config/WebSecurityConfig.java
@@ -0,0 +1,80 @@
+package com.learn.reggie.config;
+
+import com.learn.reggie.service.UserService1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private MyAccessDeniedHandler myAccessDeniedHandler;
+    @Autowired
+    private UserService1 userService;
+    @Autowired
+    private DataSource datasource;
+    @Autowired
+    private PersistentTokenRepository tokenRepository;
+
+    @Bean
+    public PasswordEncoder pw(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        .headers().frameOptions().sameOrigin();
+
+        http.formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/login")
+//                .defaultSuccessUrl("/")
+//                .successForwardUrl("/success")
+                .successHandler(new MyAuthenticationSuccessHandler("/backend/index.html"))
+//                .failureForwardUrl("/error")
+                .failureHandler(new MyAuthenticationFailureHandler("/login.html"))
+                .usernameParameter("username")
+                .passwordParameter("password");
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/login.html").permitAll()
+                .antMatchers("/backend/page/**").authenticated()
+                .antMatchers("/backend/api/**").authenticated()
+                .antMatchers("/backend/index.html").authenticated()
+                .anyRequest().permitAll();
+
+        http.exceptionHandling()
+                .accessDeniedHandler(myAccessDeniedHandler);
+
+        http.rememberMe()
+                .userDetailsService(userService)
+                .tokenRepository(tokenRepository);
+//                .rememberMeServices();
+        
+        http.logout()
+                .logoutSuccessUrl("/login.html")
+                .logoutUrl("/logout");
+
+
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository(){
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(datasource);
+//        jdbcTokenRepository.setCreateTableOnStartup(true);
+        return jdbcTokenRepository;
+    }
+}
